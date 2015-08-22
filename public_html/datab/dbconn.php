c8d3acfa32f5496f66bb9427e8aba41cd4cb2d7f
ShubhamKale <kaleshubham46@gmail.com>
2015-08-22 10:46:36 +0530

    delete old

--- public_html/datab/dbconn.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$cnobj=mysql_connect("localhost","recodoad_jlive","12243648") or die(mysql_errno());
-      $status=mysql_select_db("recodoad_jlive",$cnobj) or die(mysql_error());
-     
-?>
-			
